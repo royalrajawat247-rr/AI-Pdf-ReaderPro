df45e1a207578805faabcff5b51c60c6b444c792
royalrajawat247-rr <royalrajawat247@gmail.com>
2026-09-24 16:11:16 +0530

    Update Constants.kt

--- app/src/main/java/com/rejowan/pdfreaderpro/util/Constants.kt
+++ app/src/main/java/com/rejowan/pdfreaderpro/util/Constants.kt
@@ -1,8 +1,9 @@
 package com.rejowan.pdfreaderpro.util
 
 object Constants {
-    const val CONTACT_EMAIL = "kmrejowan@gmail.com"
-    const val SOURCE_CODE_URL = "https://github.com/ahmmedrejowan/PDF-Reader-Pro"
-    const val PORTFOLIO_URL = "https://rejowan.com"
-    const val COMPANY_NAME = "K M Rejowan Ahmmed"
+    const val CONTACT_EMAIL = "royalrajawat247@gmail.com"
+    const val SOURCE_CODE_URL = "https://github.com/royalrajawat247-rr/Al-Pdf-ReaderPro"
+    const val PORTFOLIO_URL = "https://github.com/royalrajawat247-rr"
+    const val COMPANY_NAME = "Royal Rajawat"
 }
+
